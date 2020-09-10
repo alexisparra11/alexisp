0e166cdabf9c11fc325115369d3b3fff23e1224b
alexisparra11 <cesar.parra@campusucc.edu.co>
2020-09-10 17:44:27 -0500

    se modifica java

--- alexisp.java
+++ alexisp.java
@@ -1,6 +1,13 @@
+public class vehiculos{
+    //atributos
+    String color;
+    String marca;
+    int km;
 public static void main(String[] args) {
-    System.out.println("Hola mundo");
-//
-
+    vehiculos auto1 = new vehiculos();
+    auto1.color = "gris"2222222
+    //display
+    System.out.println("El color es:"+auto1.color);
+}
 }
 
